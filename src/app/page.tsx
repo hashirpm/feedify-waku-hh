7cad13f3bb6014f1dc1a2c00e6b7b4f5fae7d20a
shiyasmohd <shiyasjaseena124@gmail.com>
2023-12-06 20:43:20 +0530

    web3.storage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { Input } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <ConnectButton />
+      <div className='max-w-[400px]'>
+        <Input type='file' />
+      </div>
     </main>
   )
 }
